46467365991334c6d9a5e8f880a0e2fc841d9780
Fruit <boskozarich@gmail.com>
2018-12-09 00:29:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const    Command = require('command');
-
-module.exports = function gourd(dispatch) {
-    const command = Command(dispatch)
+module.exports = function gourd(mod) {
     let enabled = true,
         userName = "",
-        region = dispatch.base.region;
+        region = mod.base.region;
     
     switch(region){
         case "na":
@@ -24,23 +21,23 @@ module.exports = function gourd(dispatch) {
         break;
     }
     
-    command.add(['mongord','mg'], () => {
+    mod.command.add(['mongord','mg'], () => {
         enabled = !enabled;
-        command.message(`mongord enabled: ${enabled}`);
+        mod.command.message(`mongord enabled: ${enabled}`);
     });
     
-    dispatch.hook('S_LOGIN', 10, event => {
+    mod.hook('S_LOGIN', 10, event => {
         userName = event.name;
     });
     
-    dispatch.hook('S_USER_PAPERDOLL_INFO', 5, event => {
+    mod.hook('S_USER_PAPERDOLL_INFO', 5, event => {
         if(enabled && event.name != userName){
             Open(event.name);
         }
     });
     
     function Open(name){
-        dispatch.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
+        mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
             link:`https://moongourd.com${region}results?player=${name}&area=1&boss=1&sort=timedesc`
         })
     }
